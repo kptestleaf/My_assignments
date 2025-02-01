97b27e7be2684936ed0a0eacbfbfdfdecdffaa66
kptestleaf <lathakrishnaprasath@gmail.com>
2025-02-01 20:42:43 +0530

    Add files via upload
    day1

---
+++ day1/Datatype.java
@@ -0,0 +1,22 @@
+package Week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float valueofversion = 91.0f;
+        String developer = "Google";
+        boolean isBeta = flase;
+        short releaseYear = 2008;
+        char shortcutKey = 'C';
+        
+        System.out.println(valueofversion);
+        System.out.println(developer);
+        System.out.println(isBeta);
+        System.out.println(releaseYear);
+        System.out.println(shortcutKey);
+                
+        }
+
+}
---
+++ day1/FirstProgram1.java
@@ -0,0 +1,9 @@
+package Week1.day1;
+
+public class FirstProgram1 {
+
+	public static void main (String[]args) {
+		System.out.println("Hello to TestLeaf");
+	}
+	
+}
